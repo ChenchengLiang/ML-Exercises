6d435ec46eda2dcbe88e65d7cf4377dce5b0b8c4
lcckkkhaha <lcckkkhaha@163.com>
2019-02-08 15:37:04 +0100

    Update
    Update commands pip3 to python3

--- Environment-Ubuntu/Packet install.sh
+++ Environment-Ubuntu/Packet install.sh
@@ -3,30 +3,47 @@
 #Newest Eclipse http://www.eclipse.org/downloads/
 #Different Pydev versions http://www.pydev.org/update_sites/
 
+#Anaconda https://www.anaconda.com/distribution/#download-section
+
+
+# JAVA
+sudo apt install default-jdk            
+sudo apt install openjdk-11-jdk-headless
+sudo apt install ecj                    
+sudo apt install openjdk-8-jdk-headless 
+sudo apt install openjdk-9-jdk-headless 
+
+Anaconda
+#Jupyter with pip
+python3 -m pip install --upgrade pip
+python3 -m pip install jupyter
+
+jupyter notebook
+
+
 
 sudo apt-get install gedit -y
 sudo apt-get install python3-pip -y
 
 #Basics
-pip3 install scipy
-pip3 install numpy
-pip3 install pandas
-pip3 install h5py
+sudo python3 -m pip install numpy
+sudo python3 -m pip install scipy
+sudo python3 -m pip install pandas
+sudo python3 -m pip install h5py
+
 
 #Graphs
-pip3 install mathplotlib
-pip3 install mathplotlib
-pip3 install seaborn
-pip3 install pyglet
+sudo python3 -m pip install mathplotlib
+sudo python3 -m pip install seaborn
+sudo python3 -m pip install pyglet
 
 
 #Frame work
 
-
-pip3 install theano
-pip3 install tensorflow
-pip3 install keras
-pip3 install scikit-learn
-pip3 install scikit-neuralnetwork
-pip3 install sklearn
-pip3 install xgboost
+sudo python3 -m pip install theano
+sudo python3 -m pip install tensorflow
+sudo python3 -m pip install keras
+sudo python3 -m pip install scikit-learn
+sudo python3 -m pip install scikit-neuralnetwork
+sudo python3 -m pip install sklearn
+sudo python3 -m pip install  xgboost
